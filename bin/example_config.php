293954d75a4792f27e5c363d5f6ad9e1e0f0f980
DocTolly <deren93@hotmail.com>
2013-02-11 15:53:26 +0000

    Update bin/example_config.php
    merged ServerCon into config

--- bin/example_config.php
+++ bin/example_config.php
@@ -1,11 +1,35 @@
 <?php
-$db = array();
-$db['host'] = 'localhost'; // MySQL Server Host
-$db['port'] = '3306'; // MySQL Server Port
-$db['user'] = "dayz"; // MySQL Username
-$db['password'] = "yourpassword"; // MySQL Password
-$db['database'] = "dayz"; // MySQL Database
+/*
+DatabaseConnection
 
+Connects script to MySQL database. Edit the configuration below
+*/
+class DatabaseConnection {
+
+  //Server host or IP:Port  if remotely connecting
+  private $db_host = "localhost";
+  //Database Username
+  private $db_user = "";
+  //Database Password
+	private $db_pass = "";
+  //Database name
+	private $db_name = "dayz";
+
+	function __construct() {
+		$this->connect();
+	}
+	public function connect()
+	{
+		mysql_connect($this->db_host,$this->db_user,$this->db_pass) or die(mysql_error());
+		$this->selectDatabase($this->db_name);
+	}
+	public function selectDatabase($db) {
+		mysql_select_db($db);
+	}
+}
+/*
+Configuration
+*/
 $config = array();
 $config['instance'] = 1; // Instance number
 $config['icons'] = true;
