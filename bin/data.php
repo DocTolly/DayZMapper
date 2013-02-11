736972c9692a24d3fb4d030124c649fa6a4fa360
DocTolly <deren93@hotmail.com>
2013-02-11 16:11:18 +0000

    Update bin/data.php

--- bin/data.php
+++ bin/data.php
@@ -7,7 +7,7 @@ header('Content-Type: text/plain');
 header('Cache-Control: no-cache, must-revalidate'); // HTTP/1.1
 header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // Date in the past
 
-require_once 'ServerCon.php';
+require_once 'config.php';
 $db = new DatabaseConnection();
 
 echo '<stuff>' . "\n";
