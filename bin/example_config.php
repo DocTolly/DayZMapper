275c0cb1f919a7b9c0a7f9adf4c1f72561b15d1d
DocTolly <deren93@hotmail.com>
2013-02-11 15:54:52 +0000

    Update bin/example_config.php

--- bin/example_config.php
+++ bin/example_config.php
@@ -11,22 +11,26 @@ class DatabaseConnection {
   //Database Username
   private $db_user = "";
   //Database Password
-	private $db_pass = "";
+  private $db_pass = "";
   //Database name
-	private $db_name = "dayz";
+  private $db_name = "dayz";
 
-	function __construct() {
-		$this->connect();
-	}
-	public function connect()
-	{
-		mysql_connect($this->db_host,$this->db_user,$this->db_pass) or die(mysql_error());
-		$this->selectDatabase($this->db_name);
-	}
-	public function selectDatabase($db) {
-		mysql_select_db($db);
-	}
+  function __construct()
+  {
+    $this->connect();
+  }
+  
+  public function connect()
+  {
+    mysql_connect($this->db_host,$this->db_user,$this->db_pass) or die(mysql_error());
+    $this->selectDatabase($this->db_name);
+  }
+  
+  public function selectDatabase($db) {
+    mysql_select_db($db);
+  }
 }
+
 /*
 Configuration
 */
